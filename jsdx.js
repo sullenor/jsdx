b9c7f3db9e08591c26473e63eea604f23561f6c1
Alexey Litvinov <sullenor@yandex-team.ru>
2014-08-17 21:32:29 +0400

    Формирует АСД

--- jsdx.js
+++ jsdx.js
@@ -3,15 +3,28 @@
 var files = require('./lib/files');
 var vow = require('vow');
 
+function name(pathName) {
+    return String(pathName.match(/\/([a-z\.\-]+)$/i)[1]);
+}
+
 /**
  * @param  {(string|string[])} nodes
  * @return {object}
  */
 module.exports = function (nodes) {
     Array.isArray(nodes) || (nodes = [nodes]);
-    var ast = {};
+    var ast = {nodes: []};
 
-    return vow.all(nodes.map(function (node) {
-        return files(node);
-    }));
+    return vow.all(nodes.map(function (nodePath) {
+        var node = {name: name(nodePath), path: nodePath};
+        return files(nodePath)
+            .then(function (list) {
+                node.content = list;
+                ast.nodes.push(node);
+                return node;
+            });
+    }))
+        .then(function () {
+            return ast;
+        });
 };
