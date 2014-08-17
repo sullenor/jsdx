572b905d7ae43f234fd57734768b3aa4c4e5af71
Alexey Litvinov <sullenor@yandex-team.ru>
2014-08-17 22:22:18 +0400

    Поправил синтаксис вызова команд

--- bin/cli.js
+++ bin/cli.js
@@ -1,43 +1,57 @@
 #!/usr/bin/env node
 'use strict';
 
-var _ = require('lodash');
-var jsdx = require('../jsdx');
 var pkg = require('../package.json');
 var program = require('commander');
-var path = require('path');
-var utils = require('../lib/utils');
-var vow = require('vow');
-
-// jsdx make <path>
-// jsdx make --config <path>
 
 program
     .version(pkg.version)
-    .option('-C, --config <path>', 'specify config file')
-    .parse(process.argv);
+    .option('-c, --config <path>', 'specify config file');
 
-var configPath = path.resolve(program.config || 'jsdx.json');
-var projectPath = path.resolve('package.json');
+program
+    .command('make')
+    .description('builds documentation')
+    .action(function () {
+        var _ = require('lodash');
+        var jsdx = require('../jsdx');
+        var path = require('path');
+        var utils = require('../lib/utils');
+        var vow = require('vow');
 
-if (program.args[0] === 'make') {
-    utils.fileExists(configPath, true)
-        .then(function () {
-            var config = require(configPath);
-            return vow.all([jsdx(config.nodes), utils.fileExists(projectPath)])
-                .then(utils.splat(function (ast, haspkg) {
-                    var project = {description: '', name: 'Документация по проекту'};
-                    if (haspkg) {
-                        project = _.assign(project, _.pick(require(projectPath)));
-                    }
+        var configPath = path.resolve(program.config || 'jsdx.json');
+        var projectPath = path.resolve('package.json');
 
-                    return _.assign(ast, project);
-                }));
-        })
-        .then(function (ast) {
-            console.log(ast);
-        })
-        .catch(function (msg) {
-            console.error(msg);
-        });
+        utils.fileExists(configPath, true)
+            .then(function () {
+                var config = require(configPath);
+                return vow.all([jsdx(config.nodes), utils.fileExists(projectPath)])
+                    .then(utils.splat(function (ast, haspkg) {
+                        var project = {description: '', name: 'Документация по проекту'};
+                        if (haspkg) {
+                            project = _.assign(project, _.pick(require(projectPath)));
+                        }
+
+                        return _.assign(ast, project);
+                    }));
+            })
+            .then(function (ast) {
+                console.log(ast);
+                process.exit(0);
+            })
+            .catch(function (msg) {
+                console.error(msg);
+                process.exit(1);
+            });
+    });
+
+program
+    .command('*')
+    .action(function () {
+        program.outputHelp();
+    });
+
+program.parse(process.argv);
+
+if (program.args.length === 0) {
+    program.outputHelp();
 }
