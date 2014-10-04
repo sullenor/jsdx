e54e76ef6faf23fc334536b379532ee315ff63ff
Alexey Litvinov <iamhoneyboy@gmail.com>
2014-10-04 23:12:00 +0400

    Парсеры

--- static/js/core.js
+++ static/js/core.js
@@ -3,6 +3,8 @@
 
     var BlockModel = Backbone.Model.extend({
         initialize: function (ast) {
+            ast.js && this.set('js', processJs(ast.js));
+            ast.md && this.set('md', processMd(ast.md));
             delete ast.path;
 
             return ast;
@@ -173,7 +175,7 @@
 
             e.preventDefault();
 
-            this.main.html(JSON.stringify(data));
+            this.main.html(data);
         },
 
         /**
@@ -198,9 +200,114 @@
                 this.nav.append(new Tab('js').el);
             }
 
+            this.main.html(model.get('md'));
+
             return this;
         }
     });
 
     new App();
+
+    function processJs(ast) {
+        if (!ast) {
+            return '';
+        }
+
+        var buf = [];
+
+        buf.push('<h3>' + ast.block + '</h3>');
+
+        if (ast.baseBlock) {
+            buf.push('<p>Наследуется от блока <b>' + ast.baseBlock + '</b>.</p>');
+        }
+
+        if (ast.blockMethods.length) {
+            buf.push('<h4>Методы блока</h4>');
+            ast.blockMethods.forEach(function (method) {
+                buf.push('<h5>' + method.method + '()</h5>');
+                if (method.comment) {
+                    var comment = method.comment;
+                    if (comment.description) {
+                        buf.push('<p>' + comment.description + '</p>');
+                    }
+                    if (comment.tags.length) {
+                        buf.push('<ul>');
+                        comment.tags.forEach(function (tag) {
+                            buf.push('<li>');
+                            buf.push(tag.tag + ' ');
+                            buf.push('<code>' + tag.types + '</code>' + ' ');
+                            tag.name && buf.push(tag.name + ' ');
+                            tag.description && buf.push(tag.description);
+                            buf.push('</li>');
+                        });
+                        buf.push('</ul>');
+                    }
+                }
+            });
+        }
+
+        if (ast.staticMethods.length) {
+            buf.push('<h4>Статические методы</h4>');
+            ast.staticMethods.forEach(function (method) {
+                buf.push('<h5>' + method.method + '()</h5>');
+                if (method.comment) {
+                    var comment = method.comment;
+                    if (comment.description) {
+                        buf.push('<p>' + comment.description + '</p>');
+                    }
+                    if (comment.tags.length) {
+                        buf.push('<ul>');
+                        comment.tags.forEach(function (tag) {
+                            buf.push('<li>');
+                            buf.push(tag.tag + ' ');
+                            buf.push('<code>' + tag.types + '</code>' + ' ');
+                            tag.name && buf.push(tag.name + ' ');
+                            tag.description && buf.push(tag.description);
+                            buf.push('</li>');
+                        });
+                        buf.push('</ul>');
+                    }
+                }
+            });
+        }
+
+        return buf.join('');
+    }
+
+    function processMd(ast) {
+        if (!ast) {
+            return '';
+        }
+
+        var buf = [];
+
+        if (ast.mods) {
+            buf.push('<h4>Модификаторы блока</h4>');
+            buf.push('<ul>');
+            ast.mods.forEach(function (mod) {
+                buf.push('<li>');
+                buf.push(mod.name);
+                if (mod.vals) {
+                    buf.push('<ul>');
+                    mod.vals.forEach(function (val) {
+                        buf.push('<li>' + val.name + '</li>');
+                    });
+                    buf.push('</ul>');
+                }
+                buf.push('</li>');
+            });
+            buf.push('</ul>');
+        }
+
+        if (ast.elems) {
+            buf.push('<h4>Элементы блока</h4>');
+            buf.push('<ul>');
+            ast.elems.forEach(function (elem) {
+                buf.push('<li>' + elem.name + '</li>');
+            });
+            buf.push('</ul>');
+        }
+
+        return buf.join('');
+    }
 })();
