031894fb47eb8a2e4f1f0e8d939c4b1306ad6491
Alexey Litvinov <iamhoneyboy@gmail.com>
2014-11-23 23:46:23 +0300

    Добавил опцию silent

--- bin/jsdx.js
+++ bin/jsdx.js
@@ -7,6 +7,7 @@ program
     .option('-c, --coverage', '')
     .option('-h, --html <path>', '')
     .option('-m, --mask <pattern>', '')
+    .option('-s, --silent', 'Suppress warnings.')
     .option('-l, --lint', '')
     .option('-t, --threshold', '')
     .parse(process.argv);
