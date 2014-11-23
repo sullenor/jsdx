7f99f8eaee0ac87bc9091efe72426f4c6fc6f7b5
Alexey Litvinov <iamhoneyboy@gmail.com>
2014-11-23 23:42:06 +0300

    Возможный апи

---
+++ bin/jsdx.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var program = require('commander');
+
+program
+    .option('-c, --coverage', '')
+    .option('-h, --html <path>', '')
+    .option('-m, --mask <pattern>', '')
+    .option('-l, --lint', '')
+    .option('-t, --threshold', '')
+    .parse(process.argv);
+
+program.help();
