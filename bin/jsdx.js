ce30b14a9495b29486dd38bbff662d7c228d3d04
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-01-12 22:27:25 +0300

    Чуть более детальное описание параметров

--- bin/jsdx.js
+++ bin/jsdx.js
@@ -5,13 +5,15 @@ var program = require('commander');
 var pkg = require('../package');
 
 program
-    .version(pkg.version)
-    .option('-c, --coverage', '')
-    .option('-h, --html <path>', '')
-    .option('-m, --mask <pattern>', '')
-    .option('-s, --silent', 'Suppress warnings.')
-    .option('-l, --lint', '')
-    .option('-t, --threshold', '')
-    .parse(process.argv);
+  .version(pkg.version)
+  .option('-c, --coverage', '')
+  .option('-h, --html <path>', 'Build and deploy html report.')
+  .option('-m, --mask <pattern>', 'Sets custom mask for the readme files.')
+  .option('-s, --silent', 'Suppress warnings.')
+  .option('-l, --lint', '')
+  .option('-t, --threshold <number>', 'Sets custom threshold for the linter.')
+  .parse(process.argv);
 
-program.help();
+if (program.args.length === 0) {
+  program.help();
+}
